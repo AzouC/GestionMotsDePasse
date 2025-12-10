84edcf9baa1319c4e23ae37b60ca55523c3cba66
AzouC <153391817+AzouC@users.noreply.github.com>
2025-12-10 11:11:11 +0100

    Update PasswordVault.h

--- include/PasswordVault.h
+++ include/PasswordVault.h
@@ -2,29 +2,22 @@
 #define PASSWORDVAULT_H
 
 #include <vector>
-#include "PasswordEntry.h" // Nécessaire car le vecteur contient des objets PasswordEntry
+#include "PasswordEntry.h"
 
 class PasswordVault {
 private:
-    // Conteneur dynamique stockant les objets PasswordEntry
-    // Correspond à l'attribut "taches" de la classe ListeDeTaches [cite: 351]
     std::vector<PasswordEntry> entries;
 
 public:
-    // Constructeur
     PasswordVault();
 
-    // Ajoute une nouvelle entrée dans le coffre
-    // Passage par référence comme recommandé dans le PDF [cite: 386]
     void addEntry(const PasswordEntry& entry);
-
-    // Supprime une entrée selon son index
     void deleteEntry(int index);
-
-    // Affiche toutes les entrées stockées
-    void listEntries() const;
     
-    // (Optionnel) Retourne la taille pour des vérifications
+    // Méthode de mise à jour (Update)
+    void updateEntry(int index, const PasswordEntry& newEntry);
+
+    void listEntries() const;
     int size() const;
 };
 
