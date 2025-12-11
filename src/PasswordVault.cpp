ce2d143a57cc087409b39cd9d424c2909b88bf43
AzouC <153391817+AzouC@users.noreply.github.com>
2025-12-11 17:07:24 +0100

    Update PasswordVault.cpp

--- src/PasswordVault.cpp
+++ src/PasswordVault.cpp
@@ -12,35 +12,45 @@ void PasswordVault::addEntry(const PasswordEntry& entry) {
 void PasswordVault::deleteEntry(int index) {
     if (index >= 0 && index < (int)entries.size()) {
         entries.erase(entries.begin() + index);
-        cout << ">> Entree supprimee avec succes." << endl;
-    } else {
-        cout << ">> Erreur : Index invalide." << endl;
-    }
+        cout << ">> Entree supprimee." << endl;
+    } else cout << ">> Erreur Index." << endl;
 }
 
-// Mise à jour d'une entrée existante
 void PasswordVault::updateEntry(int index, const PasswordEntry& newEntry) {
     if (index >= 0 && index < (int)entries.size()) {
-        entries[index] = newEntry; // Remplacement dans le vecteur
-        cout << ">> Entree mise a jour avec succes." << endl;
-    } else {
-        cout << ">> Erreur : Index invalide." << endl;
-    }
+        entries[index] = newEntry;
+        cout << ">> Mise a jour OK." << endl;
+    } else cout << ">> Erreur Index." << endl;
 }
 
-void PasswordVault::listEntries() const {
-    if (entries.empty()) {
-        cout << ">> Aucun mot de passe enregistre." << endl;
-        return;
-    }
+// MODIF ICI : Gestion de l'affichage masqué/clair
+void PasswordVault::listEntries(string currentUser, bool isAdmin, bool showClear) const {
+    bool found = false;
+    cout << "\n--- COFFRE FORT (" << currentUser << ") ---" << endl;
+    
     for (size_t i = 0; i < entries.size(); ++i) {
-        PasswordEntry current = entries[i];
-        cout << "[" << i << "] Site: " << current.getSiteName() 
-             << " | User: " << current.getUsername() 
-             << " | Pass: ********" << endl;
+        if (isAdmin || entries[i].getOwner() == currentUser) {
+            cout << "ID [" << i << "] "; 
+            if (isAdmin) cout << "[Proprio: " << entries[i].getOwner() << "] ";
+            
+            cout << "Site: " << entries[i].getSiteName() 
+                 << " | User: " << entries[i].getUsername() 
+                 << " | Pass: ";
+            
+            // C'est ici que la magie opère
+            if (showClear) {
+                cout << entries[i].getPassword(); // Affiche en clair
+            } else {
+                cout << "********"; // Masque par défaut
+            }
+            cout << endl;
+            
+            found = true;
+        }
     }
+    if (!found) cout << ">> Aucune entree visible." << endl;
 }
 
-int PasswordVault::size() const {
-    return (int)entries.size();
-}
+int PasswordVault::size() const { return (int)entries.size(); }
+vector<PasswordEntry> PasswordVault::getAllEntries() const { return entries; }
+void PasswordVault::loadEntries(const vector<PasswordEntry>& loadedEntries) { entries = loadedEntries; }
