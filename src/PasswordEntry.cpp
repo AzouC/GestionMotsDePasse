a664ae385826d1c0801fe29c1525decc66078177
AzouC <153391817+AzouC@users.noreply.github.com>
2025-12-11 17:07:08 +0100

    Update PasswordEntry.cpp

--- src/PasswordEntry.cpp
+++ src/PasswordEntry.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 PasswordEntry::PasswordEntry() {
+    this->owner = "";
     this->siteName = "";
     this->username = "";
     this->password = "";
 }
 
-PasswordEntry::PasswordEntry(string site, string user, string pass) {
+PasswordEntry::PasswordEntry(string owner, string site, string user, string pass) {
+    this->owner = owner;
     this->siteName = site;
     this->username = user;
     this->password = pass;
@@ -17,7 +19,9 @@ PasswordEntry::PasswordEntry(string site, string user, string pass) {
 string PasswordEntry::getSiteName() const { return this->siteName; }
 string PasswordEntry::getUsername() const { return this->username; }
 string PasswordEntry::getPassword() const { return this->password; }
+string PasswordEntry::getOwner() const { return this->owner; }
 
 void PasswordEntry::setSiteName(string site) { this->siteName = site; }
 void PasswordEntry::setUsername(string user) { this->username = user; }
 void PasswordEntry::setPassword(string pass) { this->password = pass; }
+void PasswordEntry::setOwner(string owner) { this->owner = owner; }
